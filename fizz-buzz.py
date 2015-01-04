766a2c54a293007bd655651bc7ff865f5358012b
Moss Banay <mossbanay@gmail.com>
2015-01-04 11:37:55 +1100

    Fixed and refactored fizz-buzz.py

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,18 +1,19 @@
 import sys
 
+def map_fizzbuzz(number, fizz_number, buzz_number):
+	if (number%fizz_number==0 and number%buzz_number==0):
+		return "FB"
+	if (number%fizz_number==0):
+		return "F"
+	if (number%buzz_number==0):
+		return "B"
+	return str(number)
+
 def fizzbuzz(fizz_number, buzz_number, limit):
-	#game = range(1, limit+1)
-	game = list(map(lambda x: str(x), range(1, limit+1)))
+	# Map map function with lambda to a range array.
+	solution = map(lambda x: map_fizzbuzz(x, fizz_number, buzz_number), range(1,limit+1))
 
-	for index, element in enumerate(game):
-		if(index+1 % fizz_number == 0 and index+1 % buzz_number == 0):
-			game[index] = "FB"
-		elif(index+1 % fizz_number == 0):
-			game[index] = "F"
-		elif(index+1 % buzz_number == 0):
-			game[index] = "B"
-
-	return ' '.join(game)
+	return ' '.join(list(solution))
 
 with open(sys.argv[1]) as input_file:
 	for game in input_file.readlines():
